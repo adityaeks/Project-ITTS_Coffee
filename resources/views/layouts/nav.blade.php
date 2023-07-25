deced520ebaef8285f13a1b125fb265c5a87db06
BerrylMondow <BerrylMondow@gmail.com>
2023-07-25 07:24:01 +0700

    P

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="{{ Vite::asset('resources/css/style.css') }}">
+<div class="nav-image">
+<center>
+    <span>
+    <img class="images" src="{{ Vite::asset('resources/images/itts-coffee.svg') }}">
+</span>
+</center>
+</div>
+<nav class="navbar">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Drink</a></li>
+            <li><a href="#">Snack</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">User</a></li>
+        </ul>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Judul Welcome
+Route::get('/', [HomeController::class, 'index']);
