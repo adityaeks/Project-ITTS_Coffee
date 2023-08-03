ce4656e994ce23ea4492eeccaecc9801840bd078
adityaeks <mohadityaekosaputra11@gmail.com>
2023-08-03 14:00:57 +0700

    Final Admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>tes</h1>
+@if(session('message'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    {{ session('message') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+    <h1>tes</h1>
 @endsection
